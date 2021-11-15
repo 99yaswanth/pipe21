5df4b3c5db801f14a0f11095a38292edc98679a4
99yaswanth <yashwanth24k@gmail.com>
2021-11-15 22:39:41 +0530

    cloning a repo

---
+++ jenkinsfile_build.groovy
@@ -0,0 +1,20 @@
+//Declerative pipeline
+pipeline{
+    agent any
+    stages{
+        stage("checkout a code"){
+            steps{
+                println "cloning a code"
+                sh "ls -l"
+                checkout([$class: 'GitSCM',branches: [[name: '*/master']], userRemoteConfigs: [[ url: 'https://github.com/99yaswanth/boxfuse-sample-java-war-hello.git']]])
+            }
+        }
+        stage("build code"){
+            steps{
+                println "clean package"
+                sh "mvn clean package"
+                sh " ls -l target/"
+            }
+        }
+    }
+}
