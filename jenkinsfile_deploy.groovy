b161bd6a5d3e7d0a836f4dbfbb253f1a9bd04ace
99yaswanth <yashwanth24k@gmail.com>
2021-11-18 15:02:27 +0530

    added server ip

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -4,6 +4,8 @@ pipeline{
     parameters{
         string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'from which branch artifacts want to deploy?')
         string(name: 'BUILD_NUM', defaultValue: '', description: 'from which BUILD NUM artifacts want to deploy?')
+        string(name: 'SERVER_IP', defaultValue: '', description: 'server ip?')
+        
     }
     stages{
         stage("download artifacts"){
@@ -13,8 +15,15 @@ pipeline{
                 aws s3 ls
                 aws s3 ls s3://yashartifacts
                 aws s3 ls s3://yashartifacts/${BRANCH_NAME}/${BUILD_NUM}/
+                aws s3 ls s3://yashartifacts/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
 
                 """
+            }
+        }
+        stage("copying artifacts"){
+            steps{
+                println "copying artifacts from jenkins to tomcat"
+                sh " ssh -i /tmp/nvirginia.pem ec2-user@${SERVER_IP} "\systemctl status tomcat\""
             }
         }
     }
