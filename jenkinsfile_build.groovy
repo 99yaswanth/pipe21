5726d5076b4be9a02e7a63401b61a7d1d51f938e
99yaswanth <yashwanth24k@gmail.com>
2021-11-18 13:41:44 +0530

    added deployment file

--- jenkinsfile_build.groovy
+++ jenkinsfile_build.groovy
@@ -16,6 +16,7 @@ pipeline{
             }
 
         }
+        
         stage("build code"){
             
             steps{
---
+++ jenkinsfile_deploy.groovy
@@ -0,0 +1,19 @@
+//descriptive pipeline
+pipeline{
+    agent any
+    parameters{
+        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'from which branch artifacts want to deploy?')
+        string(name: 'BUILD_NUM', defaultValue: ' ', description: 'from which build num artifacts want to deploy?')
+    }
+    stages{
+        stage("download artifacts"){
+            steps{
+                println "Iam downloading artifacts from s3"
+                sh """
+                aws s3 ls
+                aws s3 ls s3://yashwanth12
+                aws s3 ls s3://yashwanth12/${BRANCH_NAME}/${BUILD_NUM}/
+            }
+        }
+    }
+}
