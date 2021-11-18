11d0bc98055573a1a19e971680cce3e15f63d025
99yaswanth <yashwanth24k@gmail.com>
2021-11-18 16:28:50 +0530

    modified deploy file

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -1,30 +1,31 @@
 //descriptive pipeline
 pipeline{
     agent any
-    parameters{
-        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'from which branch artifacts want to deploy?')
-        string(name: 'BUILD_NUM', defaultValue: '', description: 'from which BUILD NUM artifacts want to deploy?')
-        string(name: 'SERVER_IP', defaultValue: '', description: 'server ip?')
-        
-    }
-    stages{
-        stage("download artifacts"){
-            steps{
-                println "Iam downloading artifacts from s3"
-                sh """
-                aws s3 ls
-                aws s3 ls s3://yashwanth12
-                aws s3 ls s3://yashwanth12/${BRANCH_NAME}/${BUILD_NUM}/
-                
+   parameters {
+    string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'From which branch artifacts want to deploy?')
+    string(name: 'BUILD_NUM', defaultValue: '', description: 'From which build number artifacts want to deploy?')
+    string(name: 'SERVER_IP', defaultValue: '', description: 'To  which want to deploy?')
 
-                """
+  }
+    stages{
+        stage("download artifact"){
+            steps {
+                println "Here I'm downloading artifacts from S3"
+                sh """
+                        aws s3 ls
+                        aws s3 ls s3://yashwanth12
+                        aws s3 ls s3://yashwanth12/${BRANCH_NAME}/${BUILD_NUM}/
+                        aws s3 cp s3://yashwanth12/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
+ 
+                   """
+                
             }
         }
-        stage("copying artifacts"){
-            steps{
-                println "copying artifacts from jenkins to tomcat"
-                sh "ssh -i /tmp/nvirginia.pem ec2-user@${SERVER_IP}"
-                sh "scp -i /tmp/nvirginia.pem hello-*.war ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps"
+        stage("copy artifacts") {
+            steps {
+                println "Here I'm coping artifact from Jenkins to Tomcat servers"
+                sh "ssh -i /tmp/nvirginia.pem ec2-user@${SERVER_IP} \"systemctl status tomcat\""
+                sh "scp -i /tmp/nvirginia.pem hello-${BUILD_NUM}.war  ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps"
             }
         }
     }
