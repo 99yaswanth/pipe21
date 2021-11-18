3c5398fa4563cd449ca80afe3db33914c02c4f66
99yaswanth <yashwanth24k@gmail.com>
2021-11-18 14:24:22 +0530

    added comment

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -3,7 +3,7 @@ pipeline{
     agent any
     parameters{
         string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'from which branch artifacts want to deploy?')
-        string(name: 'BUILD_NUM', defaultValue: '', description: 'from which build num artifacts want to deploy?')
+        //string(name: 'BUILD_NUM', defaultValue: '', description: 'from which build num artifacts want to deploy?')
     }
     stages{
         stage("download artifacts"){
@@ -12,7 +12,7 @@ pipeline{
                 sh """
                 aws s3 ls
                 aws s3 ls s3://yashartifacts
-                aws s3 ls s3://yashartifacts/${BRANCH_NAME}/${BUILD_NUM}/
+                aws s3 ls s3://yashartifacts/${BRANCH_NAME}/
 
                 """
             }
