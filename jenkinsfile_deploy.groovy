42aab5baf2ba1167362f7d1deea3c46b5a8dc624
99yaswanth <yashwanth24k@gmail.com>
2022-04-07 08:44:31 +0530

    dep

--- jenkinsfile_deploy.groovy
+++ jenkinsfile_deploy.groovy
@@ -13,9 +13,9 @@ pipeline{
                 println "Here I'm downloading artifacts from S3"
                 sh """
                         aws s3 ls
-                        aws s3 ls s3://yashwanth24
-                        aws s3 ls s3://yashwanth24/${BRANCH_NAME}/${BUILD_NUM}/
-                        aws s3 cp s3://yashwanth24/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
+                        aws s3 ls s3://yashwanth12
+                        aws s3 ls s3://yashwanth12/${BRANCH_NAME}/${BUILD_NUM}/
+                        aws s3 cp s3://yashwanth12/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
  
                    """
                 
@@ -24,8 +24,8 @@ pipeline{
         stage("copy artifacts") {
             steps {
                 println "Here I'm coping artifact from Jenkins to Tomcat servers"
-                sh "ssh -i /tmp/nvirginia.pem ec2-user@${SERVER_IP} \"systemctl status tomcat\""
-                sh "scp -i /tmp/nvirginia.pem hello-${BUILD_NUM}.war  ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps"
+                 sh "scp -o StrictHostKeyChecking=no -i tmp/yashnv.pem target/hello-${BUILD_NUM}.war ec2-user@${SERVER_IP}:/tmp"
+                 sh "ssh -o StrictHostKeyChecking=no -i /tmp/yashnv.pem ec2-user@${SERVER_IP} \"sudo cp /tmp/hello-${BUILD_NUM}.war /var/lib/tomcat/webapps\""
                
             }
         }
