56dc25ef08869c3fe4e6776a0cbc6ce510096047
99yaswanth <yashwanth24k@gmail.com>
2022-04-07 08:14:04 +0530

    bui

--- Jenkinsfile_bui.groovy
+++ Jenkinsfile_bui.groovy
@@ -1,9 +1,9 @@
 pipeline{
     agent any
     parameters{
-        string(name: 'branch', defaultValue: '')
-        string(name: 'buildno', defaultValue: '')
-        string(name: 'serverip', defaultValue: '')
+        string(name: 'branch', defaultValue: '', description: '')
+        string(name: 'buildno', defaultValue: '', description: '')
+        string(name: 'serverip', defaultValue: '', description: '')
     }
     stages{
         stage("clone a code"){
@@ -23,11 +23,11 @@ pipeline{
                 sh "aws s3 cp target/hello-${buildno}.war s3://yashwanth12/${branch}/${buildno}/"
             }
         }
-        stage("deploy to tomcat"){
-            steps{
-                sh "scp -o StrictHostKeyChecking=no -i tmp/yashnv.pem target/hello-${buildno}.war ec2-user@${serverip}:/tmp"
-                sh "ssh -o StrictHostKeyChecking=no -i /tmp/yashnv.pem ec2-user@${serverip} \"sudo cp /tmp/hello-${buildno}.war /var/lib/tomcat/webapps\""
-            }
-        }
+        // stage("deploy to tomcat"){
+        //     steps{
+        //         sh "scp -o StrictHostKeyChecking=no -i tmp/yashnv.pem target/hello-${buildno}.war ec2-user@${serverip}:/tmp"
+        //         sh "ssh -o StrictHostKeyChecking=no -i /tmp/yashnv.pem ec2-user@${serverip} \"sudo cp /tmp/hello-${buildno}.war /var/lib/tomcat/webapps\""
+        //     }
+        // }
     }
 }
